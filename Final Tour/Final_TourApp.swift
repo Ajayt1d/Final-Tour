e78ee57d454639ce1b567bde1e17e5c8708e4b1f
AJ <aidenjgrant@gmail.com>
2024-11-16 21:28:09 +0000

    Initial Commit

---
+++ Final Tour/Final_TourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_TourApp.swift
+//  Final Tour
+//
+//  Created by AJAY Grant on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Final_TourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Final TourTests/Final_TourTests.swift
@@ -0,0 +1,17 @@
+//
+//  Final_TourTests.swift
+//  Final TourTests
+//
+//  Created by AJAY Grant on 16/11/2024.
+//
+
+import Testing
+@testable import Final_Tour
+
+struct Final_TourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
